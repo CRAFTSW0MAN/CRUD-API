93289146a85612fbc2baee92dca75a50e65e0c58
craftsw0man <craftsw0man@yandex.ru>
2026-03-19 23:59:25 +0600

    feat: add function for post

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,65 @@
-import Fastify from 'fastify';
-import dotenv from 'dotenv';
+import Fastify from "fastify";
+import dotenv from "dotenv";
+import { z } from "zod";
+import { randomUUID } from "node:crypto";
 
+const ProductBodySchema = z
+  .object({
+    id: z.string().min(1).optional(),
+    name: z.string().min(1),
+    description: z.string().min(1),
+    price: z.number().positive(),
+    category: z.string().min(1),
+    inStock: z.boolean(),
+  })
+  .strict();
+
+const ProductSchema = z
+  .object({
+    id: z.string().min(1),
+    name: z.string().min(1),
+    description: z.string().min(1),
+    price: z.number().positive(),
+    category: z.string().min(1),
+    inStock: z.boolean(),
+  })
+  .strict();
+
+type ProductBody = z.infer<typeof ProductBodySchema>;
+type Product = z.infer<typeof ProductSchema>;
 dotenv.config();
 
-const PORT = parseInt(process.env.PORT || '4000', 10);
+const products: Product[] = [];
 
-const server = Fastify({
-  logger: true
+const PORT: number = parseInt(process.env.PORT || "4000", 10);
+
+const server: Fastify.FastifyInstance = Fastify({
+  logger: true,
 });
 
-server.get('/api/products', async (request, reply) => {
-  return { message: 'Сервер работает! Список продуктов пока пуст' };
+server.get("/api/products", async (request, reply) => {
+  return reply.send(products);
 });
 
-const start = async () => {
+server.post("/api/products", async (request, reply):Promise<void> => {
+  const result = ProductBodySchema.safeParse(request.body);
+  if (!result.success) {
+    return reply.status(400).send({ message: "Invalid input" });
+  }
+
+  const newProduct: Product = {
+    id: randomUUID(),
+    name: result.data.name,
+    description: result.data.description,
+    price: result.data.price,
+    category: result.data.category,
+    inStock: result.data.inStock,
+  };
+  products.push(newProduct);
+  return reply.status(201).send(newProduct);
+});
+
+const start = async () :Promise<void>=> {
   try {
     await server.listen({ port: PORT });
     console.log(`Сервер запущен на http://localhost:${PORT}`);
